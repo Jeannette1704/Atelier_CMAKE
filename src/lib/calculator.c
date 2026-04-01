c37ab6858dcac909aa02fd68a273c0fe9879b0c8
Jeannette1704 <jeannette.titiana@2028.icam.fr>
2026-04-01 13:31:07 +0200

    Fix _car function to divide a by itself

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -15,6 +15,6 @@ double _mul(double a, double b) {
 double _div(double a, double b) {
 	return a/b;
 }
-double _car(double a, double b) {
-	return a/b;
+double _car(double a, double a) {
+	return a/a;
 }
